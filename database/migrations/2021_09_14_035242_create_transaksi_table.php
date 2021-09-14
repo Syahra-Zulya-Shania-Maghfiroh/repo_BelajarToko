9b4ac372d45b769a09160bd2ab72a6721f0ffd35
Syahra-Zulya-Shania-Maghfiroh <syahrazulyashaniamaghfiroh@gmail.com>
2021-09-14 12:20:40 +0700

    mencoba

---
+++ database/migrations/2021_09_14_035242_create_transaksi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->bigIncrements('id_transaksi');
+            $table->unsignedBigInteger('id_pelanggan');
+
+            $table->foreign('id_pelanggan')->references('id_pelanggan')->on('pelanggan');
+            
+            $table->date('tgl_transaksi');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
